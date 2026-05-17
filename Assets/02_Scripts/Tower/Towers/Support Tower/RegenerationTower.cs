5566cb361cd05b93aafb1c5064bc46623204099f
keastmin <kemin1910@gmail.com>
2026-05-17 20:10:31 +0900

    feat: 재생 타워 기능 복구

--- Assets/02_Scripts/Tower/Towers/Support Tower/RegenerationTower.cs
+++ Assets/02_Scripts/Tower/Towers/Support Tower/RegenerationTower.cs
@@ -1,18 +1,33 @@
 using Fusion;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class RegenerationTower : SupportTower
 {
+    [Header("Heal")]
+    [SerializeField] private LayerMask _detectLayer;
+    [SerializeField] private float _healRange = 25f;
     [SerializeField] private float _armor = 100f;
     [SerializeField] private float _maxHeal = 100f;
     [SerializeField] private float _healPerSec = 5f;
 
-    private bool _receiveArmor = false;
-    private IHeal _detectedRunner;
-    private IHeal _enteringRunner;
+    private Collider[] _detectedHealReceivers;
+    private HashSet<IHeal> _armorReceivedHeals;
+    private HashSet<IHeal> _currentFrameHeals;
+    private float _remainingHeal;
 
-    private float _healTimer = 1f;
-    private float _currentTimer = 0f;
+    protected override void TowerAwake()
+    {
+        _detectedHealReceivers = new Collider[100];
+        _armorReceivedHeals = new HashSet<IHeal>();
+        _currentFrameHeals = new HashSet<IHeal>();
+    }
+
+    public override void Spawned()
+    {
+        base.Spawned();
+        _remainingHeal = _maxHeal;
+    }
 
     protected override void TowerFixedUpdateNetwork()
     {
@@ -21,20 +36,57 @@ public class RegenerationTower : SupportTower
             return;
         }
 
-        if (!TryGetGrid(out InfiniteGrid gm))
+        if (_remainingHeal <= 0f)
         {
-            _detectedRunner = null;
-            _enteringRunner = null;
-            _receiveArmor = false;
-            _currentTimer = 0f;
+            DespawnRegenerationTower();
             return;
         }
 
+        int detectedCount = Physics.OverlapSphereNonAlloc(transform.position, _healRange, _detectedHealReceivers, _detectLayer);
+        _currentFrameHeals.Clear();
+        for (int i = 0; i < detectedCount; i++)
+        {
+            if (!_detectedHealReceivers[i].TryGetComponent(out IHeal heal))
+            {
+                continue;
+            }
+
+            if (!_currentFrameHeals.Add(heal))
+            {
+                continue;
+            }
+
+            if (_armorReceivedHeals.Add(heal))
+            {
+                heal.ReceiveArmor(_armor);
+            }
+
+            float healAmount = Mathf.Min(_healPerSec * Runner.DeltaTime, _remainingHeal);
+            float actualHealedAmount = heal.Heal(healAmount);
+            if (actualHealedAmount <= 0f)
+            {
+                continue;
+            }
+
+            _remainingHeal = Mathf.Max(0f, _remainingHeal - actualHealedAmount);
+            if (_remainingHeal <= 0f)
+            {
+                DespawnRegenerationTower();
+                break;
+            }
+        }
     }
 
     protected override void TowerDespawned()
     {
         base.TowerDespawned();
+        _armorReceivedHeals?.Clear();
+    }
+
+    private void DespawnRegenerationTower()
+    {
+        ReleaseGridOccupation();
+        Runner.Despawn(Object);
     }
 }
 

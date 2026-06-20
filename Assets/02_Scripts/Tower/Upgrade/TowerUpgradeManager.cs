587c17946bffeb5168cf885506957dec0f024e91
keastmin <kemin1910@gmail.com>
2026-06-20 17:48:42 +0900

    feat: 타워 업그레이드 시 호스트에게 변수 변경 요청 추가, 횟수 카운트 반영 추가
    1. 타워를 업그레이드 하면 호스트에게 업그레이드 횟수 변수를 변경할 것을 요청하고 반영까지 기다리는 로직이 추가됨
    2. UI 프리팹에 현재 강화 횟수 텍스트를 추가하고 현재 강화 횟수가 반영되도록 함

--- Assets/02_Scripts/Tower/Upgrade/TowerUpgradeManager.cs
+++ Assets/02_Scripts/Tower/Upgrade/TowerUpgradeManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 namespace KIM.Dev {
     public class TowerUpgradeManager : NetworkBehaviour
     {
+        public event Action<TowerUpgradeType, bool, int> OnTowerUpgradeRequestCompleted;
+
         [SerializeField] private int _maxSentryGunUpgradeCount;
         [SerializeField] private int _maxLaserBeamUpgradeCount;
         [SerializeField] private int _maxMisileRauncherUpgradeCount;
@@ -14,22 +16,24 @@ namespace KIM.Dev {
         [SerializeField] private int _maxSparkUpgradeCount;
         [SerializeField] private int _maxBiohazardUpgradeCount;
 
-        [Networked]
+        [Networked, OnChangedRender(nameof(SentryGunUpgradeCountChange))]
         private int _currentSentryGunUpgradeCount { get; set; }
-        [Networked]
+        [Networked, OnChangedRender(nameof(LaserBeamUpgradeCountChange))]
         private int _currentLaserBeamUpgradeCount { get; set; }
-        [Networked]
+        [Networked, OnChangedRender(nameof(MisileRauncherUpgradeCountChange))]
         private int _currentMisileRauncherUpgradeCount { get; set; }
-        [Networked]
+        [Networked, OnChangedRender(nameof(RailGunUpgradeCountChange))]
         private int _currentRailGunUpgradeCount { get; set; }
-        [Networked]
+        [Networked, OnChangedRender(nameof(BladeUpgradeCountChange))]
         private int _currentBladeUpgradeCount { get; set; }
-        [Networked]
+        [Networked, OnChangedRender(nameof(PlasmaUpgradeCountChange))]
         private int _currentPlasmaUpgradeCount { get; set; }
-        [Networked]
+        [Networked, OnChangedRender(nameof(SparkUpgradeCountChange))]
         private int _currentSparkUpgradeCount { get; set; }
-        [Networked]
+        [Networked, OnChangedRender(nameof(BiohazardUpgradeCountChange))]
         private int _currentBiohazardUpgradeCount { get; set; }
+
+        public event Action<TowerUpgradeType, int> OnTowerUpgradeCountChange;
 
         public override void Spawned()
         {
@@ -38,12 +42,20 @@ namespace KIM.Dev {
 
         public bool TryTowerUpgrade(TowerUpgradeType type)
         {
+            if (!Enum.IsDefined(typeof(TowerUpgradeType), type))
+                return false;
+
             if (!IsCanUpgrade(type))
                 return false;
 
-            TowerUpgradeCountUp(type);
+            RPC_RequestTowerUpgradeCountUp(type);
 
             return true;
+        }
+
+        public bool IsUpgradeCountReflected(TowerUpgradeType type, int upgradeCount)
+        {
+            return GetCurrentUpgradeCount(type) >= upgradeCount;
         }
 
         private void InitializeTowerUpgradeManager()
@@ -66,7 +78,7 @@ namespace KIM.Dev {
             return GetCurrentUpgradeCount(type) < GetMaxUpgradeCount(type);
         }
 
-        private int GetCurrentUpgradeCount(TowerUpgradeType type)
+        public int GetCurrentUpgradeCount(TowerUpgradeType type)
         {
             return type switch
             {
@@ -100,15 +112,6 @@ namespace KIM.Dev {
 
         private void TowerUpgradeCountUp(TowerUpgradeType type)
         {
-            RPC_RequestTowerUpgradeCountUp(type);
-        }
-
-        [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
-        private void RPC_RequestTowerUpgradeCountUp(TowerUpgradeType type)
-        {
-            if (!HasStateAuthority)
-                return;
-
             switch (type)
             {
                 case TowerUpgradeType.SentryGun:
@@ -138,6 +141,92 @@ namespace KIM.Dev {
                 default:
                     throw new ArgumentOutOfRangeException(nameof(type), type, null);
             }
+        }
+
+        [Rpc(RpcSources.All, RpcTargets.StateAuthority)]
+        private void RPC_RequestTowerUpgradeCountUp(TowerUpgradeType type, RpcInfo rpcInfo = default)
+        {
+            bool isValidType = Enum.IsDefined(typeof(TowerUpgradeType), type);
+            bool isUpgradeAccepted = isValidType && IsCanUpgrade(type);
+
+            if (isUpgradeAccepted)
+                TowerUpgradeCountUp(type);
+
+            int currentUpgradeCount = isValidType ? GetCurrentUpgradeCount(type) : 0;
+            SendTowerUpgradeResult(rpcInfo.Source, type, isUpgradeAccepted, currentUpgradeCount);
+        }
+
+        private void SendTowerUpgradeResult(
+            PlayerRef requester,
+            TowerUpgradeType type,
+            bool isUpgradeAccepted,
+            int currentUpgradeCount)
+        {
+            if (requester == PlayerRef.None)
+            {
+                NotifyTowerUpgradeRequestCompleted(type, isUpgradeAccepted, currentUpgradeCount);
+                return;
+            }
+
+            RPC_NotifyTowerUpgradeResult(requester, type, isUpgradeAccepted, currentUpgradeCount);
+        }
+
+        [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
+        private void RPC_NotifyTowerUpgradeResult(
+            [RpcTarget] PlayerRef requester,
+            TowerUpgradeType type,
+            bool isUpgradeAccepted,
+            int currentUpgradeCount)
+        {
+            NotifyTowerUpgradeRequestCompleted(type, isUpgradeAccepted, currentUpgradeCount);
+        }
+
+        private void NotifyTowerUpgradeRequestCompleted(
+            TowerUpgradeType type,
+            bool isUpgradeAccepted,
+            int currentUpgradeCount)
+        {
+            OnTowerUpgradeRequestCompleted?.Invoke(type, isUpgradeAccepted, currentUpgradeCount);
+        }
+
+        private void SentryGunUpgradeCountChange()
+        {
+            OnTowerUpgradeCountChange?.Invoke(TowerUpgradeType.SentryGun, _currentSentryGunUpgradeCount);
+        }
+
+        private void LaserBeamUpgradeCountChange()
+        {
+            OnTowerUpgradeCountChange?.Invoke(TowerUpgradeType.LaserBeam, _currentLaserBeamUpgradeCount);
+        }
+
+        private void MisileRauncherUpgradeCountChange()
+        {
+            OnTowerUpgradeCountChange?.Invoke(TowerUpgradeType.MisileRauncher, _currentMisileRauncherUpgradeCount);
+        }
+
+        private void RailGunUpgradeCountChange()
+        {
+            OnTowerUpgradeCountChange?.Invoke(TowerUpgradeType.RailGun, _currentRailGunUpgradeCount);
+        }
+
+        private void BladeUpgradeCountChange()
+        {
+            OnTowerUpgradeCountChange?.Invoke(TowerUpgradeType.Blade, _currentBladeUpgradeCount);
+        }
+
+        private void PlasmaUpgradeCountChange()
+        {
+            OnTowerUpgradeCountChange?.Invoke(TowerUpgradeType.Plasma, _currentPlasmaUpgradeCount);
+        }
+
+        private void SparkUpgradeCountChange()
+        {
+            OnTowerUpgradeCountChange?.Invoke(TowerUpgradeType.Spark, _currentSparkUpgradeCount);
+        }
+
+        private void BiohazardUpgradeCountChange()
+        {
+            OnTowerUpgradeCountChange?.Invoke(TowerUpgradeType.Biohazard, _currentBiohazardUpgradeCount);
         }
     }
 }

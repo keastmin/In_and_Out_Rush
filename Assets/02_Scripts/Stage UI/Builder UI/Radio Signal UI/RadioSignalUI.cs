4d36ef59d59df3bff54c308abd271fae61346d52
keastmin <kemin1910@gmail.com>
2026-07-20 21:10:05 +0900

    무전 신호 기반 추가, 맵 크기 변경

---
+++ Assets/02_Scripts/Stage UI/Builder UI/Radio Signal UI/RadioSignalUI.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace KIM.Dev
+{
+    public class RadioSignalUI : MonoBehaviour
+    {
+        [SerializeField] private SignalItem _signalItemPrefab;
+
+        public void InitializeRadioSignalUI()
+        {
+
+        }
+    }
+}
